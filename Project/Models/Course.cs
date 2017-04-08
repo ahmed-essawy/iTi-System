26e92a9380b96c03fef5f13f2998493e96baebff
Sawsan <Sawsan.m@yahoo.com>
2017-04-08 21:14:08 +0200

    Course class

---
+++ Project/Models/Course.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Project.Models
+{
+    public class Course
+    {
+        [Key]
+        [Required]
+        public string Code { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+        public int LectDuration { get; set; }
+        public int LabDuration { get; set; }
+        public virtual ICollection<Department> Departments { get; set; }
+        public virtual  ICollection<Instructor> Instructors { get; set; }
+    }
+}
