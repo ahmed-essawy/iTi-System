6c0899e410b5af8cf23bbe47c2b83a6768976f56
User <User@DESKTOP-LV2C8DT>
2017-04-08 20:37:34 +0200

    Vacation class

---
+++ Project/Models/Vacation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Project.Models
+{
+    public class Vacation
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required,StringLength(50)]
+        public string Name { get; set; }
+        [Required]
+        public DateTime StartDate { get; set; }
+        [Required]
+        public DateTime EndDate { get; set; }
+        
+
+
+    }
+}
