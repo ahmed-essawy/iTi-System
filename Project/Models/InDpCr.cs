eb9d07639c33bea550a8257d7f92a5c921eed31b
Sawsan <Sawsan.m@yahoo.com>
2017-04-08 21:14:35 +0200

    Instructor, Department and Course relation class

---
+++ Project/Models/InDpCr.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Project.Models
+{
+    public class InDpCr
+    {
+        [Key]
+        public int DpId { get; set; }
+        [Key]
+        public int InId { get; set; }
+        [Key]
+        public string CrCode { get; set; }
+        public virtual Department Department { get; set; }
+        public virtual Instructor Instructor { get; set; }
+        public virtual Course Course { get; set; }
+    }
+}
