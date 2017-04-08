70e61e07780ef8041af17790d65d3c30d15c6d9e
Linah <li.s.elmansy@gmail.com>
2017-04-08 21:02:25 +0200

    Exam class

---
+++ Project/Models/Exam.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Project.Models
+{
+    public class Exam
+    {   [Key]
+        public int Id { get; set; }
+        [Required]
+        public float Duration { get; set; }
+        [Required]
+        public string Subject { get; set; }
+        [Required]
+        public int From { get; set; }
+        [Required]
+        public int To { get; set; }
+        public virtual ICollection<Question> Questions { get; set; }
+        public virtual ICollection<Course> Course { get; set; }
+    }
+}
