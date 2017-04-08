8891fbdd4e05ea1413b37ded86a8aaa72287d952
Linah <li.s.elmansy@gmail.com>
2017-04-08 21:02:39 +0200

    Question class

---
+++ Project/Models/Question.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Project.Models
+{
+    public class Question
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string  Header { get; set; }
+        [Required]
+        [StringLength(500)]
+        public string Body { get; set; }
+        [Required]
+        [StringLength(500)]
+        public List<string> Answers { get; set; }
+        [Required]
+        [StringLength(500)]
+        public string CorrectAnswer { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Subject { get; set; }
+        public virtual ICollection<Exam> Exams { get; set; }
+
+
+
+    }
+}
