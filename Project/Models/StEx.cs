46467772e5249642bb46513405f6913575390680
Linah <li.s.elmansy@gmail.com>
2017-04-08 21:03:00 +0200

    Student and Exam class

---
+++ Project/Models/StEx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Project.Models
+{
+    public class StEx
+    {
+        [ForeignKey("Exam")]
+        public int ExId { get; set; }
+        [ForeignKey("Students")]
+        public int StId { get; set; }
+        [Required]
+        public float Grade { get; set; }
+        public virtual Exam Exam { get; set; }
+        public virtual ICollection<Student> Students { get; set; }
+    }
+}
