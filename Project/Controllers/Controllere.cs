827f535944bf2210ea2b92430f510692d055e5de
Ahmed El-Essawy <ahm.elessawy@gmail.com>
2017-04-11 18:49:11 +0200

    Controllere Class

--- Project/Controllers/Controllere.cs
+++ Project/Controllers/Controllere.cs
@@ -1,21 +1,24 @@
 ﻿using System.Web;
 using System.Web.Mvc;
 using Microsoft.AspNet.Identity.Owin;
+using Project.Models;
 
 namespace Project.Controllers
 {
-    public class Sign : Controller
+    public class Controllere : Controller
     {
+        public ApplicationDbContext DB = new ApplicationDbContext();
+
         private ApplicationSignInManager _signInManager;
         private ApplicationUserManager _userManager;
 
-        public ApplicationSignInManager In
+        public ApplicationSignInManager SignIn
         {
             get { return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>(); }
             private set { _signInManager = value; }
         }
 
-        public ApplicationUserManager Up
+        public ApplicationUserManager SignUp
         {
             get { return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>(); }
             private set { _userManager = value; }
