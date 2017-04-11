e2ff3c8bb1b376edfbc96c0c9f47c3d173f056c7
Ahmed El-Essawy <ahm.elessawy@gmail.com>
2017-04-11 18:38:17 +0200

    Add Signin & Signup Class

---
+++ Project/Controllers/SignController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity.Owin;
+
+namespace Project.Controllers
+{
+    public class Sign : Controller
+    {
+        private ApplicationSignInManager _signInManager;
+        private ApplicationUserManager _userManager;
+
+        public ApplicationSignInManager In
+        {
+            get { return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>(); }
+            private set { _signInManager = value; }
+        }
+
+        public ApplicationUserManager Up
+        {
+            get { return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>(); }
+            private set { _userManager = value; }
+        }
+    }
+}
