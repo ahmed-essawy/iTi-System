15b7d9e4d0b0b6e29ce4cc77da26bbee63d287b6
Ahmed El-Essawy <ahm.elessawy@gmail.com>
2017-04-12 02:19:56 +0200

    Attendance Structure

---
+++ Project/Controllers/AttendanceController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Project.Models;
+
+namespace Project.Controllers
+{
+    public class AttendanceController : Controllere
+    {
+        // GET: Attendance
+        public ActionResult Index()
+        {
+            return View(DB.Attendaces);
+        }
+
+        public ActionResult Create()
+        {
+            foreach (Student std in DB.Students)
+                if (DB.Attendaces.Where(d => d.StudentId == std.Id).Where(d => d.Date.Day == DateTime.Now.Day).Where(d => d.Date.Month == DateTime.Now.Month).Count(d => d.Date.Year == DateTime.Now.Year) <= 0)
+                    DB.Attendaces.Add(new Attendance(std.Id));
+            DB.SaveChanges();
+            return Json("Success", JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Display()
+        {
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Arrive(string Id)
+        {
+            DB.Attendaces.FirstOrDefault(d => d.StudentId == Id).ArrivalTime = DateTime.Now;
+            DB.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Leave(string Id)
+        {
+            DB.Attendaces.FirstOrDefault(d => d.StudentId == Id).LeavingTime = DateTime.Now;
+            DB.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
--- Project/Models/Attendance.cs
+++ Project/Models/Attendance.cs
@@ -1,22 +1,36 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Data;
 
 namespace Project.Models
 {
     [Table("Attendances")]
     public class Attendance
     {
+        public Attendance()
+        {
+        }
+
+        public Attendance(string Id)
+        {
+            Date = DateTime.Now;
+            StudentId = Id;
+        }
+
         [Key, Column(Order = 0)]
+        [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
         public DateTime Date { set; get; }
 
         [Key, Column("St_Id", Order = 1), ForeignKey("Student")]
         public string StudentId { get; set; }
 
-        [Column("Arrival")]
+        [Column("Arrival"), Display(Name = "Arrival Time")]
+        [DisplayFormat(DataFormatString = "{0:T}", ApplyFormatInEditMode = true)]
         public DateTime? ArrivalTime { set; get; }
 
-        [Column("Leaving")]
+        [Column("Leaving"), Display(Name = "Leaving Time")]
+        [DisplayFormat(DataFormatString = "{0:T}", ApplyFormatInEditMode = true)]
         public DateTime? LeavingTime { set; get; }
 
         public virtual Student Student { get; set; }
