4ac49747c4dddbf48bef4180a168ed3dc9631b02
Ahmed El-Essawy <ahm.elessawy@gmail.com>
2017-04-16 03:07:37 +0200

    Setup configrations

--- Project/App_Start/BundleConfig.cs
+++ Project/App_Start/BundleConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace Project
 {
@@ -9,7 +8,8 @@ namespace Project
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery.unobtrusive-ajax.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
